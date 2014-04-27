77277310112e826b3b0269cd73dc877d6a0a4830
MrPoxipol <michal.proszek@gmail.com>
2014-04-27 22:09:23 +0200

    Topic support

--- plugins/logger.js
+++ plugins/logger.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var util = require('util');
 var mkdirp = require('mkdirp');
+var mustache = require('mustache');
 
 var config = require('../config.json');
 
@@ -32,13 +33,20 @@ function addLeadingZero(date) {
 	return date;
 }
 
-function writeToFile(channel, who, message) {
+function getFileName() {
 	var date = new Date();
 	var fileName = util.format('%s_%s_%s.txt',
 		date.getUTCFullYear(),
 		date.getUTCMonth() + 1, // Month +1 because Jan = 0, Feb = 1
 		date.getUTCDate()
 	);
+
+	return fileName;
+}
+
+function writeToFile(channel, who, message) {
+	var date = new Date();
+	var fileName = getFileName();
 	var text = util.format('%s:%s <%s> %s',
 		addLeadingZero(date.getUTCHours()),
 		addLeadingZero(date.getUTCMinutes()),
@@ -51,11 +59,11 @@ function writeToFile(channel, who, message) {
 	fs.appendFileSync(getDirPath(channel) + '/' + fileName, text);
 }
 
-exports.onPluginInit = function () {
+exports.onPluginInit = function (bot) {
 
 };
 
-exports.onBotJoin = function (channel) {
+exports.onBotJoin = function (bot, channel) {
 	// Create channel folder
 	// If folder named channel doesn't exit create it 
 	var dirPath = getDirPath(channel);
@@ -66,6 +74,19 @@ exports.onBotJoin = function (channel) {
 	writeToFile(channel, me, '[JOINED] to the channel ' + channel);
 };
 
-exports.onUserJoin = function (channel, user) {
+exports.onTopic = function (bot, channel, topic, user, message) {
+	var pattern = '** Topic for {{&channel}}: {{&topic}} by {{&user}}\n';
+	var data = {
+		channel: channel,
+		topic: topic,
+		user: user
+	};
+	var output = mustache.render(pattern, data);
+
+	fileName = getFileName();
+	fs.appendFileSync(getDirPath(channel) + '/' + fileName, output);
+};
+
+exports.onUserJoin = function (bot, channel, user) {
 	writeToFile(channel, user, '[JOINED] to the channel ' + channel);
 }
