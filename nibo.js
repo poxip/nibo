8ed420aaf3ed839b7c46ace4ff916d816ea351eb
MrPoxipol <michal.proszek@gmail.com>
2014-05-05 22:38:15 +0200

    Main function

--- nibo.js
+++ nibo.js
@@ -22,6 +22,8 @@ var config = require('./nibo/config');
 
 var plugins = [];
 
+var bot;
+
 // Inform about error
 function showPluginRuntimeError(pluginName, method, exception) {
 	var data = {
@@ -121,102 +123,6 @@ function executeCallback(eventName, args) {
 	}
 }
 
-// Some config
-// FIXME: This param should be read from argv
-debug.on = config.debug;
-
-config.loadConfig();
-// Start bot = main function
-debug.log('Bot is starting up at the moment..');
-
-var bot = new irc.Client(
-	config.server.host,
-	config.bot.nick, {
-		port: config.server.port,
-		channels: config.channels,
-		stripColors: true,
-		retryCount: 5,
-		encoding: true // optional encoding - bug in nodeirc
-	}
-);
-
-bot.addListener('abort', function (retryCount) {
-	var message = util.format('Unable to connect to %s:%s',
-		config.server.host,
-		config.server.port);
-
-	debug.error(message);
-	process.exit();
-});
-
-bot.oldsay = bot.say;
-bot.say = function (target, message) {
-	bot.oldsay(target, message);
-
-	var args = {
-		channel: target,
-		message: message
-	};
-	// Bot says - event
-	executeCallback(events.botSay, args);
-};
-
-initPlugins();
-
-bot.getUser = function (message) {
-	if (!message) {
-		debug.warning('bot.getUser() requires 1 argument, 0 given');
-		return;
-	}
-
-	var user = {
-		nick: message.nick,
-		username: message.user,
-		host: message.host,
-		fullName: util.format('%s!%s@%s',
-			message.nick,
-			message.user,
-			message.host
-		)
-	};
-
-	// The object may be incomplete
-
-	return user;
-};
-
-bot.addListener('topic', function (channel, topic, nick, message) {
-	/*
-		If bot joins        nick = full user name e.g 'niboman!~op@ophost.eu'
-		If op changes topic nick = op nick        e.g 'niboman'
-	*/
-
-	var args = {
-		channel: channel,
-		topic: topic,
-		nick: nick,
-		message: message
-	};
-
-	executeCallback(events.topic, args);
-});
-
-bot.addListener('join', function (channel, nick, message) {
-	if (nick == bot.nick) {
-		executeCallback(events.botJoin, {
-			channel: channel
-		});
-		return;
-	}
-	var user = bot.getUser(message);
-
-	// If user join to the channel
-	executeCallback(events.userJoin, {
-		channel: channel,
-		user: user
-	});
-});
-
 function splitCommand(str, prefix) {
 	var withoutPrefix = str.split(prefix)[1]; // 'command arg1 arg2'
 	var splitted = withoutPrefix.split(' '); // ['command', 'arg1', 'arg2']
@@ -239,98 +145,6 @@ function executeCommand(user, channel, message) {
 	executeCallback(events.command, args);
 }
 
-bot.addListener('message', function (nick, channel, text, message) {
-	var user = bot.getUser(message);
-	var args = {
-		user: user,
-		channel: channel,
-		message: message.args[1]
-	};
-
-	var isCommand = S(args.message).startsWith(config.commandPrefix);
-	if (isCommand) {
-		executeCommand(args.user, args.channel, args.message);
-		return;
-	}
-
-	executeCallback(events.message, args);
-});
-
-bot.addListener('nick', function (oldnick, newnick, userChannels, message) {
-	var user = bot.getUser(message);
-	user.nick = newnick; // nick is newnick!
-	user.oldnick = oldnick;
-
-	var args = {
-		user: user,
-		channels: userChannels,
-	};
-
-	executeCallback(events.nick, args);
-});
-
-bot.addListener('part', function (channel, nick, reason, message) {
-	var user = bot.getUser(message);
-	var args = {
-		channel: channel,
-		user: user,
-		reason: reason
-	};
-
-	executeCallback(events.part, args);
-});
-
-bot.addListener('quit', function (nick, reason, channels, message) {
-	var user = bot.getUser(message);
-	var args = {
-		user: user,
-		channels: channels,
-		reason: reason
-	};
-
-	executeCallback(events.quit, args);
-});
-
-bot.addListener('kick', function (channel, nick, by, reason, message) {
-	var args = {
-		nick: nick,
-		by: by,
-		channel: channel,
-		reason: reason
-	};
-
-	executeCallback(events.kick, args);
-});
-
-function modeEvent(channel, by, mode, target, type) {
-	var args = {
-		channel: channel,
-		by: by,
-		mode: type + mode, // e.g '+m' or '-b'
-		target: target // user or undefined if +mode is performed on channel
-	};
-
-	executeCallback(events.mode, args);
-}
-
-bot.addListener('+mode', function (channel, by, mode, argument, message) {
-	modeEvent(channel, by, mode, argument, '+');
-});
-
-bot.addListener('-mode', function (channel, by, mode, argument, message) {
-	modeEvent(channel, by, mode, argument, '-');
-});
-
-bot.addListener('notice', function (channel, to, text, message) {
-	var args = {
-		channel: channel, // If notice from server - channel = undefined, if notice from user = nick (from)
-		to: to, // channel or user
-		text: text, // notice message
-	};
-
-	executeCallback(events.notice, args);
-});
-
 function tickEvent() {
 	executeCallback(events.tick, {});
 	/*	By setting new timeout
@@ -339,10 +153,200 @@ function tickEvent() {
 	setTimeout(tickEvent, config.tickTime);
 }
 
-// Setup tick event
-setTimeout(tickEvent, config.tickTime);
+function main() {
+	config.loadConfig();
+	// Start bot = main function
+	debug.log('Bot is starting up at the moment..');
 
-bot.addListener('invite', function (channel, from, message) {
-	if (config.joinOnInvite)
-		bot.join(channel);
-});
+	bot = new irc.Client(
+		config.server.host,
+		config.bot.nick, {
+			port: config.server.port,
+			channels: config.channels,
+			stripColors: true,
+			retryCount: 5,
+			encoding: true // optional encoding - bug in nodeirc
+		}
+	);
+
+	initPlugins();
+
+	bot.addListener('abort', function (retryCount) {
+		var message = util.format('Unable to connect to %s:%s',
+			config.server.host,
+			config.server.port);
+
+		debug.error(message);
+		process.exit();
+	});
+
+	bot.oldsay = bot.say;
+	bot.say = function (target, message) {
+		bot.oldsay(target, message);
+
+		var args = {
+			channel: target,
+			message: message
+		};
+		// Bot says - event
+		executeCallback(events.botSay, args);
+	};
+
+	bot.getUser = function (message) {
+		if (!message) {
+			debug.warning('bot.getUser() requires 1 argument, 0 given');
+			return;
+		}
+
+		var user = {
+			nick: message.nick,
+			username: message.user,
+			host: message.host,
+			fullName: util.format('%s!%s@%s',
+				message.nick,
+				message.user,
+				message.host
+			)
+		};
+
+		// The object may be incomplete
+
+		return user;
+	};
+
+	bot.addListener('topic', function (channel, topic, nick, message) {
+		/*
+		If bot joins        nick = full user name e.g 'niboman!~op@ophost.eu'
+		If op changes topic nick = op nick        e.g 'niboman'
+	*/
+
+		var args = {
+			channel: channel,
+			topic: topic,
+			nick: nick,
+			message: message
+		};
+
+		executeCallback(events.topic, args);
+	});
+
+	bot.addListener('join', function (channel, nick, message) {
+		if (nick == bot.nick) {
+			executeCallback(events.botJoin, {
+				channel: channel
+			});
+			return;
+		}
+		var user = bot.getUser(message);
+
+		// If user join to the channel
+		executeCallback(events.userJoin, {
+			channel: channel,
+			user: user
+		});
+	});
+
+	bot.addListener('message', function (nick, channel, text, message) {
+		var user = bot.getUser(message);
+		var args = {
+			user: user,
+			channel: channel,
+			message: message.args[1]
+		};
+
+		var isCommand = S(args.message).startsWith(config.commandPrefix);
+		if (isCommand) {
+			executeCommand(args.user, args.channel, args.message);
+			return;
+		}
+
+		executeCallback(events.message, args);
+	});
+
+	bot.addListener('nick', function (oldnick, newnick, userChannels, message) {
+		var user = bot.getUser(message);
+		user.nick = newnick; // nick is newnick!
+		user.oldnick = oldnick;
+
+		var args = {
+			user: user,
+			channels: userChannels,
+		};
+
+		executeCallback(events.nick, args);
+	});
+
+	bot.addListener('part', function (channel, nick, reason, message) {
+		var user = bot.getUser(message);
+		var args = {
+			channel: channel,
+			user: user,
+			reason: reason
+		};
+
+		executeCallback(events.part, args);
+	});
+
+	bot.addListener('quit', function (nick, reason, channels, message) {
+		var user = bot.getUser(message);
+		var args = {
+			user: user,
+			channels: channels,
+			reason: reason
+		};
+
+		executeCallback(events.quit, args);
+	});
+
+	bot.addListener('kick', function (channel, nick, by, reason, message) {
+		var args = {
+			nick: nick,
+			by: by,
+			channel: channel,
+			reason: reason
+		};
+
+		executeCallback(events.kick, args);
+	});
+
+	function modeEvent(channel, by, mode, target, type) {
+		var args = {
+			channel: channel,
+			by: by,
+			mode: type + mode, // e.g '+m' or '-b'
+			target: target // user or undefined if +mode is performed on channel
+		};
+
+		executeCallback(events.mode, args);
+	}
+
+	bot.addListener('+mode', function (channel, by, mode, argument, message) {
+		modeEvent(channel, by, mode, argument, '+');
+	});
+
+	bot.addListener('-mode', function (channel, by, mode, argument, message) {
+		modeEvent(channel, by, mode, argument, '-');
+	});
+
+	bot.addListener('notice', function (channel, to, text, message) {
+		var args = {
+			channel: channel, // If notice from server - channel = undefined, if notice from user = nick (from)
+			to: to, // channel or user
+			text: text, // notice message
+		};
+
+		executeCallback(events.notice, args);
+	});
+
+	// Setup tick event
+	setTimeout(tickEvent, config.tickTime);
+
+	bot.addListener('invite', function (channel, from, message) {
+		if (config.joinOnInvite)
+			bot.join(channel);
+	});
+}
+
+if (!module.parent) {
+	main();
+}
