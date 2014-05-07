051acfb5be080d85cb67a4483e468cd5792f106f
MrPoxipol <michal.proszek@gmail.com>
2014-05-08 00:02:48 +0200

    Help commands

---
+++ plugins/help.js
@@ -0,0 +1,91 @@
+/** Created on May 7, 2014
+ *  author: MrPoxipol
+ */
+
+var util = require('util');
+
+const COMMAND_NAME = 'help';
+
+exports.meta = {
+	name: 'help',
+	commandName: COMMAND_NAME,
+	description: 'Just helps!'
+};
+
+function getCommands() {
+	var names = [];
+	var descriptions = [];
+
+	var plugins = module.parent.exports.plugins;
+	if (!plugins) {
+		return commands;
+	}
+
+	for (var i in plugins) {
+		var pluginCommandName = plugins[i].meta.commandName;
+		if (!pluginCommandName)
+		// If plugin isn't a command
+			continue;
+		// Otherwise
+
+		var pluginDescription = plugins[i].meta.description;
+		names.push(pluginCommandName);
+		descriptions.push(pluginDescription);
+	}
+
+	var result = {
+		names: names,
+		descriptions: descriptions
+	};
+
+	return result;
+}
+
+function getCommandDescription(commandName) {
+	var commands = getCommands();
+	var index = commands.names.indexOf(commandName);
+
+	if (index === -1) {
+		return;
+	}
+
+	return commands.descriptions[index];
+}
+
+function getListOfCommands() {
+	var names = getCommands().names;
+
+	names.toString = function () {
+		return (this.length > 0 ? this.join(', ') : 'No one knows');
+	};
+
+	return names;
+}
+
+exports.onCommand = function (bot, user, channel, command) {
+	if (channel.indexOf('*') !== -1)
+		return;
+
+	if (command.name !== COMMAND_NAME)
+		return;
+	// !help
+	if (command.args.length < 1) {
+		var avCommands = getListOfCommands(bot);
+
+		var message = util.format('[HELP]: Avaiable commands: %s. Say help command_name for description of command.',
+			avCommands.toString()
+		);
+
+		return message;
+	} else {
+		var message = 'I\'ve never heard about this command!';
+
+		var name = command.args[0];
+		var description = getCommandDescription(name);
+		if (description) {
+			message = util.format('[HELP, %s]: %s', name, description);
+		}
+
+		return message;
+	}
+};
