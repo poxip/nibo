5cadaef9e4390b5a4f1c93953411c645fa991163
MrPoxipol <michal.proszek@gmail.com>
2014-06-24 22:53:54 +0200

    Remove not used and wrong callback param

--- plugins/logger.js
+++ plugins/logger.js
@@ -106,7 +106,7 @@ exports.onUserJoin = function (bot, channel, user) {
 	writeToFile(channel, user.fullName, '[JOINED] to the channel ' + channel);
 };
 
-exports.onMessage = function (bot, channel, user, text, message) {
+exports.onMessage = function (bot, channel, user, message) {
 	writeToFile(channel, user.nick, message);
 };
 
